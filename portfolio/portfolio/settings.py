d7221271ad97751254976c54473b8ec1c09f1d1c
Yair Ben David <79565335+Yair-BD@users.noreply.github.com>
2021-10-12 20:18:11 +0300

    next

--- portfolio/portfolio/settings.py
+++ portfolio/portfolio/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-2y^uqr+av8_*uo!s^lm-4t!92o!g$kh)c9^y$0q)=+6vew&s-5
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
